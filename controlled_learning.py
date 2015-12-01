4f7694f05287bfc0d307dd1b03004640720caaf5
Sean Aubin <saubin@uwaterloo.ca>
2015-11-30 21:28:02 -0500

    trying to get control

--- controlled_learning.py
+++ controlled_learning.py
@@ -27,7 +27,6 @@ num_ord_filt = OrderedDict(number_ordered.items()[:number_range])
 print(join_num)
 
 q_list = []
-ans_list = []
 for val in itertools.product(num_ord_filt, num_ord_filt):
     ans_val = num_ord_filt[val[0]] + num_ord_filt[val[1]]
     if ans_val < number_range:
@@ -36,10 +35,6 @@ for val in itertools.product(num_ord_filt, num_ord_filt):
                 (vocab.parse(val[0]).v, vocab.parse(val[1]).v)
             )
         )
-        ans_list.append(
-            vocab.parse(num_ord_filt.keys()[ans_val]).v
-        )
-        print("%s+%s=%s" %(val[0], val[1], ans_val))
 
 num_items = len(q_list)
 dt = 0.001
@@ -50,44 +45,39 @@ q_normed = q_normed/np.linalg.norm(q_normed)
 intercept = (np.dot(q_normed, q_normed.T) - np.eye(num_items)).flatten().max()
 print("Intercept: %s" % intercept)
 
-def cycle_array(x, period, dt=0.001):
-    """Cycles through the elements"""
-    i_every = int(round(period/dt))
-    if i_every != period/dt:
-        raise ValueError("dt (%s) does not divide period (%s)" % (dt, period))
-    def f(t):
-        i = int(round((t - dt)/dt))  # t starts at dt
-        return x[(i/i_every)%len(x)]
-    return f
-
-def create_env():
-    with nengo.Network(label="env") as env:
-        env.keys = nengo.Node(cycle_array(q_list, period, dt))
-        env.values = nengo.Node(cycle_array(ans_list, period, dt))
-    return env
-
 num_vocab = vocab.create_subset(num_ord_filt.keys())
 
 model = spa.SPA(vocabs=[vocab], label="Count Net", seed=0)
 n_neurons = 500
 
 with model:
-    env = create_env()
+    model.in_1 = spa.State(D)
+    model.in_2 = spa.State(D)
+
+    model.out = spa.State(D)
+    model.ans = spa.State(D)
+
     voja = nengo.Voja(post_tau=None, learning_rate=5e-2)
     adder = nengo.Ensemble(n_neurons, D*2, intercepts=[intercept]*n_neurons)
     recall = nengo.Node(size_in=D)
+    comb_node = nengo.Node(size_in=2*D)
 
-    conn_in = nengo.Connection(env.keys, adder, synapse=None,
+    conn_in = nengo.Connection(comb_node, adder, synapse=None,
                            learning_rule_type=voja)
 
     conn_out = nengo.Connection(adder, recall, learning_rule_type=nengo.PES(1e-3),
                                 function=lambda x: np.zeros(D))
 
+    nengo.Connection(model.in_1.output, comb_node[D:])
+    nengo.Connection(model.in_2.output, comb_node[:D])
+
+    nengo.Connection(recall, model.out.input)
+
     # Create the error population
     error = nengo.Ensemble(n_neurons, D)
 
     # Calculate the error and use it to drive the PES rule
-    nengo.Connection(env.values, error, transform=-1, synapse=None)
+    nengo.Connection(model.ans.output, error, transform=-1, synapse=None)
     nengo.Connection(recall, error, synapse=None)
     nengo.Connection(error, conn_out.learning_rule)
 
@@ -96,42 +86,4 @@ with model:
 
     # OH SHIT IT'S THE INTERCEPTS
     dummy = nengo.Ensemble(n_neurons, D*2, intercepts=[intercept]*n_neurons)
-    nengo.Connection(env.keys, dummy, synapse=None)
-
-
-    # Probing modes
-    if(sim_mode == "gui"):
-        model.in_1 = spa.State(D)
-        model.in_2 = spa.State(D)
-
-        model.out = spa.State(D)
-        model.ans = spa.State(D)
-
-        nengo.Connection(env.keys[D:], model.in_1.input)
-        nengo.Connection(env.keys[:D], model.in_2.input)
-        nengo.Connection(recall, model.out.input)
-        nengo.Connection(env.values, model.ans.input)
-
-    elif(sim_mode == "cli"):
-        p_in1 = nengo.Probe(env.keys[D:], synapse=None)
-        p_in2 = nengo.Probe(env.keys[:D], synapse=None)
-
-        p_out = nengo.Probe(recall, synapse=None)
-        p_ans = nengo.Probe(env.values, synapse=None)
-        p_err_mag = nengo.Probe(err_node, synapse=0.005)
-        p_err = nengo.Prove(p_err, synapse=0.005)
-
-
-if(sim_mode == "cli"):
-    sim = nengo.Simulator(model, dt=dt)
-    sim.run(T*10)
-    ipdb.set_trace()
-    import matplotlib.pyplot as plt
-
-    trange = sim.trange()
-
-    plt.plot(trange, sim.data[p_err])
-    plt.show()
-
-
-# Aside: Deleting a node that was initially typoed caused an error?
+    nengo.Connection(comb_node, dummy, synapse=None)
