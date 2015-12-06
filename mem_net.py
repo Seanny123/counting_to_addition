eba007d9005461f3d0c2903d719d47afd031ced5
Seanny123 <saubin@uwaterloo.ca>
2015-12-06 13:00:48 -0500

    IT WORKS

--- mem_net.py
+++ mem_net.py
@@ -1,10 +1,12 @@
 import nengo
+from nengo import spa
 from nengo.networks.workingmemory import InputGatedMemory as WM
 from nengo.spa.module import Module
+import numpy as np
 
 class MemNet(Module):
     def __init__(self, d, mem_vocab, label=None, seed=None, add_to_container=None):
-        super(IncNet, self).__init__(label, seed, add_to_container)
+        super(MemNet, self).__init__(label, seed, add_to_container)
 
         gate_vocab = spa.Vocabulary(16)
         gate_vocab.parse("OPEN+CLOSE")
@@ -12,10 +14,10 @@ class MemNet(Module):
         with self:
             self.mem = WM(100, d, difference_gain=15)
             self.mem.label = "mem"
-            self.gate = nengo.Node(d)
+            self.gate = nengo.Node(size_in=16)
 
             nengo.Connection(self.gate, self.mem.gate,
-                             transform=np.array([vocab.parse("OPEN").v]))
+                             transform=np.array([gate_vocab.parse("CLOSE").v]))
 
         self.inputs = dict(default=(self.mem.input, mem_vocab), gate=(self.gate, gate_vocab))
         self.outputs = dict(default=(self.mem.output, mem_vocab))
--- tri_mem.py
+++ tri_mem.py
@@ -1,5 +1,6 @@
 import nengo
 from nengo import spa
+from mem_net import MemNet
 
 import numpy as np
 from collections import OrderedDict
@@ -38,7 +39,7 @@ with model:
     # Also going to need a bit of a gate...
 
     # maybe this should be a mem block?
-    model.op_state = spa.State(less_D, vocab=state_vocab, feedback=1)
+    model.op_state = MemNet(less_D, state_vocab, label="op_state")
 
     # TODO: Make this adaptively large
     input_keys = ['ZERO', 'ONE', 'TWO', 'THREE', 'FOUR']
@@ -46,15 +47,15 @@ with model:
 
     model.res_assoc = spa.AssociativeMemory(input_vocab=num_vocab, output_vocab=num_vocab,
                                             input_keys=input_keys, output_keys=output_keys,
-                                            wta_output=True)
-    model.count_res = spa.State(D, vocab=num_vocab, feedback=1)
-    model.res_mem = spa.State(D, vocab=num_vocab, feedback=1)
+                                            wta_output=True, input_scale=2.5)
+    model.count_res = MemNet(D, num_vocab, label="count_res")
+    model.res_mem = MemNet(D, num_vocab, label="res_mem")
 
     model.tot_assoc = spa.AssociativeMemory(input_vocab=num_vocab, output_vocab=num_vocab,
                                             input_keys=input_keys, output_keys=output_keys,
-                                            wta_output=True)
-    model.count_tot = spa.State(D, vocab=num_vocab, feedback=1)
-    model.tot_mem = spa.State(D, vocab=num_vocab, feedback=1)
+                                            wta_output=True, input_scale=2.5)
+    model.count_tot = MemNet(D, num_vocab, label="count_tot")
+    model.tot_mem = MemNet(D, num_vocab, label="tot_mem")
 
     model.count_fin = spa.State(D, vocab=num_vocab, feedback=1)
 
@@ -98,26 +99,28 @@ with model:
     """
 
 
-    # Even with preset states this routing is fucked
+    # We may not actually ever need to use the gate...
     actions = spa.Actions(
-        # If the input isn't blank, read it in
+        ## If the input isn't blank, read it in
         on_input=
         "(0.5*dot(q1, %s) + 0.5*dot(q2, %s) + dot(op_state, NONE))/1.5 "
         "--> count_res = q1*ONE, count_tot = ONE, count_fin = q2, op_state = RUN" % (join_num, join_num,),
-        # If not done, prepare next increment
+        ## If not done, prepare next increment
         cmp_fail=
-        "0.75*dot(op_state, RUN) - 0.5*comp_tot_fin + comp_inc_res - 1.25*comp_load_res"
+        "dot(op_state, RUN) - 0.5*comp_tot_fin + comp_inc_res - comp_load_res"
         "--> op_state = 0.5*RUN - NONE, res_mem = count_res, tot_mem = count_tot, "
+        "count_res_gate = CLOSE, count_tot_gate = CLOSE, op_state_gate = CLOSE, "
         "comp_load_res_A = res_mem, comp_load_res_B = count_res",
-        # If we're done incrementing write it to the answer
+        ## If we're done incrementing write it to the answer
         cmp_good=
-        "0.25*dot(op_state, RUN) + comp_tot_fin"
+        "0.5*dot(op_state, RUN) + comp_tot_fin"
         "--> answer = count_res, op_state=NONE",
-        # Increment memory transfer
+        ## Increment memory transfer
         increment=
-        "0.25*dot(op_state, RUN) + 1.75*comp_load_res - comp_inc_res"
+        "0.3*dot(op_state, RUN) + 1.2*comp_load_res - comp_inc_res"
         "--> res_assoc = res_mem, tot_assoc = tot_mem, "
-        "comp_load_res_A = 0.5*ONE, comp_load_res_B = 0.5*ONE, "
+        "res_mem_gate = CLOSE, tot_mem_gate = CLOSE, op_state_gate = CLOSE, "
+        "comp_load_res_A = 0.75*ONE, comp_load_res_B = 0.75*ONE, "
         "comp_inc_res_A = res_mem*ONE, comp_inc_res_B = count_res",
     )
 
@@ -125,8 +128,8 @@ with model:
     model.thal = spa.Thalamus(model.bg)
 
     cortical_actions = spa.Actions(
-        "count_res = res_assoc",
-        "count_tot = tot_assoc",
+        "count_res = 2*res_assoc",
+        "count_tot = 2*tot_assoc",
         "comp_tot_fin_A = count_fin",
         "comp_tot_fin_B = count_tot",
     )
